29935d3705f376cf6453ac080a33b288ed8282f2
wdeeee <tranquocdectk1821@gmail.com>
2023-04-15 22:07:51 +0700

    add text file for questions

--- Lab01/6.1/src/ChoosingOption.java
+++ Lab01/6.1/src/ChoosingOption.java
@@ -2,7 +2,7 @@ import javax.swing.JOptionPane;
 public class ChoosingOption {
     public static void main(String[] args){
         int option = JOptionPane.showConfirmDialog(null,"Do you want to change to the first class ticket");
-        JOptionPane.showMessageDialog(null,"You've choosen: " + (option==JOptionPane.YES_OPTION?"Yes":"No"));
+        JOptionPane.showMessageDialog(null,"You've choosen: " + (option==JOptionPane.YES_NO_OPTION?"I do":"I do not"));
         System.exit(0);
     }   
 }
