eb8b4abed82e30c7f71cc12a1d771bb96520cc74
wdeeee <tranquocdectk1821@gmail.com>
2023-04-28 15:59:24 +0700

    fix

--- Lab02/AIMS_project/src/DigitalVideoDisc.java
+++ Lab02/AIMS_project/src/DigitalVideoDisc.java
@@ -13,26 +13,14 @@ public class DigitalVideoDisc {
     public String getCategory() {
         return category;
     }
-    public void setCategory(String category) {
-        this.category = category;
-    }
     public String getDirector() {
         return director;
-    }
-    public void setDirector(String director) {
-        this.director = director;
     }
     public int getLength() {
         return length;
     }
-    public void setLength(int length) {
-        this.length = length;
-    }
     public float getCost() {
         return cost;
-    }
-    public void setCost(float cost) {
-        this.cost = cost;
     }
     public DigitalVideoDisc(String title) {
         this.title = title;
@@ -43,21 +31,7 @@ public class DigitalVideoDisc {
         this.category = category;
         this.cost = cost;
     }
-    public DigitalVideoDisc(String title, String category, String director, float cost) {
-        this.title = title;
-        this.category = category;
-        this.director = director;
-        this.cost = cost;
-    }
-    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
-        this.title = title;
-        this.category = category;
-        this.director = director;
-        this.length = length;
-        this.cost = cost;
-    }
     public DigitalVideoDisc(float cost) {
         this.cost = cost;
     }
-
 }
